df7881ebb9179a0cf9601fa6367c695055927aac
Smith <smith@futureworkz.com>
2017-02-24 16:51:31 +0700

    Add robots

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,9 @@
 class HomeController < ApplicationController
   def index; end
+
+  def robots
+    path = Rails.root + "public/robots.#{Rails.env}.txt"
+    robots = File.exist?(path) ? File.read(path) : ''
+    render text: robots, layout: false, content_type: "text/plain"
+  end
 end
